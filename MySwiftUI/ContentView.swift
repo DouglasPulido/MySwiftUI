b824bc8081f5bce5fdfb609df9147ea12c96bbfa
douglas.araujo <douglas.araujo@foursys.com.br>
2022-06-10 07:24:36 -0700

    commit inicial

--- MySwiftUI/ContentView.swift
+++ MySwiftUI/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 500)
+            CircleImage()
+                .offset(x: 0, y: -130)
+                .padding(.bottom, -130)
+                VStack(alignment: .leading){
+                    Text("São Paulo")
+                        .font(.title)
+                    HStack(alignment: .top){
+                        Text("Parque Ibirapuera")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("São Paulo")
+                            .font(.subheadline)
+                    }
+            }
+                .padding()
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11")
     }
 }
